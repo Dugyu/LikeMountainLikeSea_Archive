7be4399f07f0b4b19810b3db2c456b483560a2ae
Dugyu <yxylyzy@126.com>
2019-07-25 10:02:57 +0800

    update sensitive fish

--- Assets/Scripts/Control/ApproachFish.cs
+++ Assets/Scripts/Control/ApproachFish.cs
@@ -7,18 +7,28 @@ public class ApproachFish : MonoBehaviour
     public GameObject fishTemplate;
     public GameObject player;
     SensitiveFish sensitiveFish;
+    List<SensitiveFish> sensitiveFishShoal = new List<SensitiveFish>();
     // Start is called before the first frame update
     void Start()
     {
-        sensitiveFish = new SensitiveFish(fishTemplate);
-        fishTemplate.GetComponent<MeshRenderer>().enabled = false;
         SensitiveFish.AddEnemy(player);
+        for (int i = 0; i < 10; i++)
+        {
+            sensitiveFish = new SensitiveFish(fishTemplate);
+            sensitiveFish.pos = Random.insideUnitSphere * 2;
+            sensitiveFishShoal.Add(sensitiveFish);
+        }
+
+        fishTemplate.GetComponent<MeshRenderer>().enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        sensitiveFish.Move();
+        foreach (SensitiveFish sensitiveFish in sensitiveFishShoal)
+        {
+            sensitiveFish.Move();
+        }
     }
 }
