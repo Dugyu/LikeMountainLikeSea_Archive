439f3b8750e0e5455084ebc418ab63043857419f
Dugyu <yxylyzy@126.com>
2019-08-08 17:33:46 +0800

    update demo scences

--- Assets/Scripts/Control/WanderFish.cs
+++ Assets/Scripts/Control/WanderFish.cs
@@ -27,7 +27,7 @@ public class WanderFish : MonoBehaviour
         }
         gegeTemplate.SetActive(false);
 
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 10; i++)
         {
             heluo = new AmiableFish(heluoTemplate);
 
