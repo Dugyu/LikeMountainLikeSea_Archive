9655615a442e76af02e53be3009f656854a2c923
Dugyu <yxylyzy@126.com>
2019-08-12 12:54:14 +0800

    update emerge scene rupi

---
+++ Assets/Scripts/Control/FishDown.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishDown : MonoBehaviour
+{
+
+    private BannerManager bannerManager;
+
+    int rupiTimer;
+    int rupiLoop;
+
+    public GameObject rupiTemplate;
+    private GameObject rupi;
+    Vector3 rupiOrigin = new Vector3(-1.0f, 1.08f, 0.152f);
+
+    private void Awake()
+    {
+        bannerManager = BannerManager.Instance;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bannerManager.InBanner && bannerManager.CurrentBanner != null)
+        {
+            string currentBanner = bannerManager.CurrentBanner;
+
+            switch (currentBanner)
+            {
+                case "rupi":
+                    RupiUpdate();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+
+
+    void RupiUpdate()
+    {
+        if (rupiTimer % 64 == 0 && rupiLoop < 20)
+        {
+            rupi = Instantiate(rupiTemplate);
+            rupi.transform.position = Random.insideUnitSphere * 0.5f + rupiOrigin;
+            rupiLoop++;
+        }
+
+        if (rupiTimer == 130)
+        {
+            rupiTimer = 0;
+
+        }
+        rupiTimer++;
+    }
+
+
+}
